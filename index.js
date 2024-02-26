d2c3e3fe23132b64992d6835b255882d2de8287a
Asrinne Nazari <asse_89@hotmail.com>
2024-02-26 09:57:08 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const {check} = require('express-validator')
+const cors = require('cors')
+const migrationhelper = require('./migrationhelper')
+const app = express()
+const port = 3000 // "Radiofrekvens"
+
+
+app.use(express.json())
+app.use(cors({
+    origin:"http://localhost:5500",
+    credentials:true 
+}))
+
+app.get('/products', async(req,res) =>{
+
+})
+
+
+
+app.listen(port, async () => {
+    await migrationhelper.migrate()
+    console.log(`Example app listening on port ${port}`)
+})
